000716d2994e07455d0961da45062094979282a3
John Ivan Puayap <johnivanpuayap@gmail.com>
2023-10-19 00:32:40 +0800

    Display movies

--- movie/models.py
+++ movie/models.py
@@ -5,15 +5,33 @@ from django.db import models
 class Genre(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']  # Change the ordering to last name and then first name
+
 
 class Director(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+    class Meta:
+        ordering = ['first_name', 'last_name', ]  # Change the ordering to last name and then first name
+
 
 class Cast(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+    class Meta:
+        ordering = ['first_name', 'last_name', ]  # Change the ordering to last name and then first name
 
 
 class Movie(models.Model):
@@ -25,11 +43,20 @@ class Movie(models.Model):
     # The project assumes that a movie can only have one director
     director = models.ForeignKey(Director, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title']
+
 
 class MovieCast(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     cast = models.ForeignKey(Cast, on_delete=models.CASCADE)
     role = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.movie.__str__() + " as " + self.role
 
     class Meta:
         unique_together = ('movie', 'cast')
--- movie/views.py
+++ movie/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.views.generic.base import View
+from .models import Movie
 
 
 class HomeView(View):
     def get(self, request):
-        return render(request, "index.html")
+        movies = Movie.objects.all()
+
+        context = {
+            'movies': movies,
+        }
+
+        return render(request, "index.html", context)
