394d93b69d00e9f8ecffb612d3c11e688c1b3009
John Ivan Puayap <johnivanpuayap@gmail.com>
2023-10-19 10:50:34 +0800

    Fix the issue where genre is not displayed and add error messages

--- movie/views.py
+++ movie/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic.base import View
 from django.db.models import Q
-from .models import Movie
+from .models import Movie, Genre
 
 
 class HomeView(View):
@@ -9,17 +9,17 @@ class HomeView(View):
         # returns all the movies
         movies = Movie.objects.all()
 
-        # returns all the movies that contains with g
-        movies = Movie.objects.filter(
-            Q(title__icontains='g')
-        )
-
-        # returns all the movies that starts with g
-        # this is case-sensitive
-        movies = Movie.objects.filter(
-            Q(title__startswith='G') |
-            Q(title__startswith='T')
-        )
+        # # returns all the movies that contains with g
+        # movies = Movie.objects.filter(
+        #     Q(title__icontains='g')
+        # )
+        #
+        # # returns all the movies that starts with g
+        # # this is case-sensitive
+        # movies = Movie.objects.filter(
+        #     Q(title__startswith='G') |
+        #     Q(title__startswith='T')
+        # )
 
         context = {
             'movies': movies,
@@ -29,11 +29,14 @@ class HomeView(View):
 
 
 def movie(request, movie_title):
+    try:
+        movie_details = get_object_or_404(Movie, title=movie_title)
 
-    movie_details = Movie.objects.get(title=movie_title)
+        context = {
+            "movie": movie_details,
+        }
 
-    context = {
-        "movie": movie_details
-    }
-
-    return render(request, "movie.html", context)
+        return render(request, "movie.html", context)
+    except Movie.DoesNotExist:
+        # Handle the case where the movie doesn't exist, e.g., redirect to an error page or display an error message.
+        return render(request, "error.html", {"error_message": "Movie not found"})
